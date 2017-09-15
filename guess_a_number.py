d91ac3a79d86bb2127d378d858db2f868b46ad0f
Gallagher <ngalla3683@greenville.k12.sc.us>
2017-09-15 09:12:50 -0400

    Check if guess was number

--- guess_a_number.py
+++ guess_a_number.py
@@ -12,10 +12,20 @@ print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 guess = -1
 tries = 0
 
+#helper functions
+def get_guess():
+    while True:
+        g = input("Take a guess: ")
+
+        if g.isnumeric():
+            g = int(g)
+            return g
+        else:
+            print("You must enter a number")
+
 #play the game
 while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
+    guess = get_guess()
     
     if guess < rand:
         print("You guessed too low.")
